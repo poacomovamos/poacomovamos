4498bf7d7f141a4fc29af1f36e6f2580d3614267
Duda Dornelles <dudassdornelles@gmail.com>
2013-05-17 11:27:50 -0300

    fixing calendario header

--- app/assets/javascripts/calendarioSessao.js
+++ app/assets/javascripts/calendarioSessao.js
@@ -2,7 +2,7 @@
 //= require 'calendar'
 
 var january2013 = calendar.for(1, 2013),
-  table = domTable.create({content: january2013, header: ['S', 'T', 'Q', 'Q', 'S', 'S', 'D']});
+  table = domTable.create({content: january2013, header: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']});
 
 $(function () {
   $("#content").append(table);
