cef861277a82882ae914bf2c2091266043250e2f
Duda Dornelles <dudassdornelles@gmail.com>
2013-05-17 10:55:19 -0300

    header for table - making it the days of week for calendar

--- app/assets/javascripts/calendarioSessao.js
+++ app/assets/javascripts/calendarioSessao.js
@@ -1,8 +1,8 @@
-//= require 'tablemaker'
+//= require 'domTable'
 //= require 'calendar'
 
-var january2013 = calendar.for(2, 2013),
-  table = tableMaker.create(january2013);
+var january2013 = calendar.for(1, 2013),
+  table = domTable.create({content: january2013, header: ['S', 'T', 'Q', 'Q', 'S', 'S', 'D']});
 
 $(function () {
   $("#content").append(table);
