45218762adcabeda6be832e3ef3208825114be2c
Glauco Vinicius <gl4uc0@gmail.com>
2013-05-28 20:01:25 -0300

    Adding the remaining columns available on 'Projetos em Tramitacao'

--- app/models/projects_in_course_scraper.rb
+++ app/models/projects_in_course_scraper.rb
@@ -3,12 +3,23 @@ require 'open-uri'
 
 class ProjectsInCourseScraper
 
-  projects_in_course_url = lambda { |page_number| "http://projetos.camarapoa.rs.gov.br/consultas/em_tramitacao?page=#{page_number}" }
+  #projects_in_course_url = lambda { |page_number| "http://projetos.camarapoa.rs.gov.br/consultas/em_tramitacao?page=#{page_number}" }
 
   def process html
     doc = Nokogiri::HTML(html)
     doc.css('.even, .odd').map do |node|
-      { number: node.css('td')[0].text }
+      {
+        number: node.css('td')[0].text.strip,
+        year: node.css('td')[1].text.strip,
+        type: node.css('td')[2].text.strip,
+        materia: node.css('td')[3].text.strip,
+        ementa: node.css('td')[4].text.strip,
+        processingDate: node.css('td')[5].text.strip,
+        sector: node.css('td')[6].text.strip,
+        authors: node.css('td')[7].text.strip,
+        status: node.css('td')[8].text.strip,
+        veto: node.css('td')[9].text.strip
+      }
     end
   end
 end
--- spec/models/projects_in_course_scraper_spec.rb
+++ spec/models/projects_in_course_scraper_spec.rb
@@ -9,10 +9,54 @@ describe ProjectsInCourseScraper do
       projects = subject.process html
 
       projects.should match_array([
-        {number: "01634"},
-        {number: "04078"},
-        {number: "01978"},
-        {number: "00674"}
+        {
+          number: "01634", 
+          year: "2012", 
+          type: "PLE", 
+          materia: "32", 
+          ementa: "declara de utilidade publica o instituto urbis porto alegre - cidadania e participacao popular.",
+          processingDate: "05/02/2013",
+          sector: "cece - comissao de educacao cultura e esportes",
+          authors: "governo municipal",
+          status: "para parecer",
+          veto: ""
+        },
+        {
+          number: "04078", 
+          year: "2011", 
+          type: "PLL", 
+          materia: "242", 
+          ementa: "inclui inc. iii no paragrafo unico do art. 42 da lei n. 6.310,  de  28  de dezembro de 1988 - que estabelece o plano de carreira dos funcionarios do departamento municipal de habitacao (demhab),  dispoe sobre o plano de pagamento e da outras providencias -, e alteracoes posteriores, incluindo excecao a proibicao de o funcionario convocado para o regime especial de dedicacao exclusiva exercer cumulativamente outro cargo, funcao ou atividade publica ou privada.",
+          processingDate: "05/02/2013",
+          sector: "cuthab - comissao de urban,transp e habitacao",
+          authors: "reginaldo pujol",
+          status: "para parecer",
+          veto: ""
+        },
+        {
+          number: "01978", 
+          year: "2009", 
+          type: "PLL", 
+          materia: "81", 
+          ementa: "inclui  art. 6-a na lei  n. 5.548, de 28 de dezembro de 1984 - que consolida dispositivos relativos a instituicao da passagem escolar no municipio de porto alegre e da outras providencias -, e alteracoes posteriores, possibilitando o uso dessa passagem em todos os dias da semana, inclusive e feriados.",
+          processingDate: "05/02/2013",
+          sector: "cefor - comissao de economia financas orcamento e",
+          authors: "fernanda melchionna",
+          status: "para parecer",
+          veto: ""
+        },
+        {
+          number: "00674", 
+          year: "2009", 
+          type: "PLL", 
+          materia: "12", 
+          ementa: "reserva 10% (dez por cento) das unidades de moradia e os apartamentos terreos as pessoas idosas e as pessoas com deficiencia contempladas como beneficiarias nos programas habitacionais populares implantados pelo executivo municipal, revoga a lei n. 10396, de 1 de abril de 2008, e da outras providencias.",
+          processingDate: "06/02/2013",
+          sector: "setor legislativo",
+          authors: "waldir canal",
+          status: "ordem do dia",
+          veto: ""
+        }
       ])
     end
   end
