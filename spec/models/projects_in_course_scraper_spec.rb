875b0919fdee18ede992f65ca7d3e66ef4d3a1ed
Diego Borges <dborges@thoughtworks.com>
2013-05-28 21:52:14 -0300

    Creates a rspec matcher to improve test readability

--- spec/models/projects_in_course_scraper_spec.rb
+++ spec/models/projects_in_course_scraper_spec.rb
@@ -2,26 +2,15 @@ require 'spec_helper'
 require 'open-uri'
 
 describe ProjectsInCourseScraper do
-  
+
   describe '#process' do
     it "extracts project's information from HTML as a Ruby hash" do
       html = open('spec/models/projects_in_course_page.html')
       projects = subject.process html
 
-      projects.should match_array([
-        {
-          number: "01634", 
-          year: "2012", 
-          type: "PLE", 
-          materia: "32", 
-          ementa: "declara de utilidade publica o instituto urbis porto alegre - cidadania e participacao popular.",
-          processingDate: "05/02/2013",
-          sector: "cece - comissao de educacao cultura e esportes",
-          authors: "governo municipal",
-          status: "para parecer",
-          veto: ""
-        },
-        {
+      projects.size.should eq 2
+
+      projects[0].should match_project_in_course({
           number: "04078", 
           year: "2011", 
           type: "PLL", 
@@ -32,32 +21,20 @@ describe ProjectsInCourseScraper do
           authors: "reginaldo pujol",
           status: "para parecer",
           veto: ""
-        },
-        {
-          number: "01978", 
-          year: "2009", 
-          type: "PLL", 
-          materia: "81", 
-          ementa: "inclui  art. 6-a na lei  n. 5.548, de 28 de dezembro de 1984 - que consolida dispositivos relativos a instituicao da passagem escolar no municipio de porto alegre e da outras providencias -, e alteracoes posteriores, possibilitando o uso dessa passagem em todos os dias da semana, inclusive e feriados.",
+      })
+
+      projects[1].should match_project_in_course({
+          number: "01634", 
+          year: "2012", 
+          type: "PLE", 
+          materia: "32", 
+          ementa: "declara de utilidade publica o instituto urbis porto alegre - cidadania e participacao popular.",
           processingDate: "05/02/2013",
-          sector: "cefor - comissao de economia financas orcamento e",
-          authors: "fernanda melchionna",
+          sector: "cece - comissao de educacao cultura e esportes",
+          authors: "governo municipal",
           status: "para parecer",
           veto: ""
-        },
-        {
-          number: "00674", 
-          year: "2009", 
-          type: "PLL", 
-          materia: "12", 
-          ementa: "reserva 10% (dez por cento) das unidades de moradia e os apartamentos terreos as pessoas idosas e as pessoas com deficiencia contempladas como beneficiarias nos programas habitacionais populares implantados pelo executivo municipal, revoga a lei n. 10396, de 1 de abril de 2008, e da outras providencias.",
-          processingDate: "06/02/2013",
-          sector: "setor legislativo",
-          authors: "waldir canal",
-          status: "ordem do dia",
-          veto: ""
-        }
-      ])
+      })
     end
   end
 end
