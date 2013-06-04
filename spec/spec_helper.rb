096739db0a89f285188ccc53d83ce5cc17b78ae3
Jefferson Girao <jgirao@thoughtworks.com>
2013-06-04 20:45:20 -0300

    +JrAles removing evil white blank trailing space (We are whitespaces NAZIs)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,7 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
-  
+
   config.include FactoryGirl::Syntax::Methods
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
