ca252b610b7cfd7345732920ca982e8442f52175
Jefferson Girao <jgirao@thoughtworks.com>
2013-06-04 20:40:50 -0300

    Removing useless code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,4 @@ class SessionsController < ApplicationController
   def sessions
     @sessions_calendar = [SessionsCalendar.for_year(2011), SessionsCalendar.for_year(2012), SessionsCalendar.for_year(2013)]
   end
-
-  def color
-    "white"
-  end
 end
