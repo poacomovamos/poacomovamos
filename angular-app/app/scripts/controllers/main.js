5e131dadeb649aa74fcf6f457ef8735cb6f608e0
drborges <dborges@thoughtworks.com>
2013-07-09 21:42:31 -0300

    Adiciona AngularJS

---
+++ angular-app/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('uiApp')
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
