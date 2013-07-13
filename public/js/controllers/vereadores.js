9f8c5cd2ba2f75a89513c13cd0a41c665883f69f
guifroes <guilherme.froes@gmail.com>
2013-07-13 00:56:55 -0300

    Concertando o erro de 10 iterations reached (retornando array de vereadores ao inves de uma function que retorna o array)

--- public/js/controllers/vereadores.js
+++ public/js/controllers/vereadores.js
@@ -7,11 +7,9 @@ poaCvApp.controller('VereadoresCtrl', function ($scope) {
       return (presencasDuranteMandato / numeroSecoesDuranteMandato) * 100;
     };
 
-    $scope.vereadores = function () {
-      return [
+    $scope.vereadores = [
         { nome: 'Airto Ferronato', email: 'ferronato@camarapoa.rs.gov.br', partido: 'PAB', foto: 'http://www.camarapoa.rs.gov.br/frames/veread/fotos/airtoferronato2.jpg', presencaUltimaSessao: true,  presencasDuranteMandato: 5 },
         { nome: 'Alceu Brasinha', email: 'brasinha@camarapoa.rs.gov.br',  partido: 'PTB', foto: 'http://www.camarapoa.rs.gov.br/frames/veread/fotos/brasinha.jpg',        presencaUltimaSessao: false, presencasDuranteMandato: 2 },
         { nome: 'Bernardino Vendruscolo', email: '', partido: 'PSD', foto: 'http://www.camarapoa.rs.gov.br/frames/veread/fotos/bernardino.jpg', presencaUltimaSessao: true, presencasDuranteMandato: 8 }
       ];
-    };
-  });
+});
