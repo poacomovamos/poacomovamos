7324f97ee87ca0958e557ef80689c895f6c2d9d7
guifroes <guilherme.froes@gmail.com>
2013-07-14 19:01:30 -0300

    Embaralhando os vereadores antes de exibir

--- public/js/controllers/vereadores.js
+++ public/js/controllers/vereadores.js
@@ -6,10 +6,24 @@ poaCvApp.controller('VereadoresCtrl', ['$scope', 'vereadores', function ($scope,
       var numeroSecoesDuranteMandato = 10;
       return (presencasDuranteMandato / numeroSecoesDuranteMandato) * 100;
     };
-    $scope.vereadores = vereadores;
 //    $scope.vereadores = [
 //        { nome: 'Airto Ferronato', email: 'ferronato@camarapoa.rs.gov.br', partido: 'PAB', foto: 'http://www.camarapoa.rs.gov.br/frames/veread/fotos/airtoferronato2.jpg', presencaUltimaSessao: true,  presencasDuranteMandato: 5 },
 //        { nome: 'Alceu Brasinha', email: 'brasinha@camarapoa.rs.gov.br',  partido: 'PTB', foto: 'http://www.camarapoa.rs.gov.br/frames/veread/fotos/brasinha.jpg',        presencaUltimaSessao: false, presencasDuranteMandato: 2 },
 //        { nome: 'Bernardino Vendruscolo', email: '', partido: 'PSD', foto: 'http://www.camarapoa.rs.gov.br/frames/veread/fotos/bernardino.jpg', presencaUltimaSessao: true, presencasDuranteMandato: 8 }
 //      ];
+    var shuffle = function(array) {
+      var newArray = array.slice();
+      var i = newArray.length, j, temp;
+    
+      while ( --i )
+      {
+        j = Math.floor( Math.random() * (i - 1) );
+        temp = newArray[i];
+        newArray[i] = newArray[j];
+        newArray[j] = temp;
+      }
+      return newArray;
+    };
+
+    $scope.vereadores = shuffle(vereadores);
 }]);
--- test/js/controllers/vereadores.js
+++ test/js/controllers/vereadores.js
@@ -17,5 +17,11 @@ describe('Controller: vereadores', function() {
 
   it('deve embaralhar todos os vereadores antes de retornar', function() {
     expect($scope.vereadores.length).toBe(6);
+    expect($scope.vereadores[0]).not.toBe('Jose Sarney');
+    expect($scope.vereadores[1]).not.toBe('Fernando Collor');
+    expect($scope.vereadores[2]).not.toBe('Itamar Franco');
+    expect($scope.vereadores[3]).not.toBe('Fernando Henrique Cardoso');
+    expect($scope.vereadores[4]).not.toBe('Luis Inacio Lula');
+    expect($scope.vereadores[5]).not.toBe('Dilma Rousseff');
   });
 });
