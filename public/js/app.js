d95ae15281e5c11872099d0ff73fa1cc67032a87
Thiago Felix <felixthi@gmail.com>
2013-07-23 20:40:40 -0300

    resolve de vereadores volta direto a promise do

--- public/js/app.js
+++ public/js/app.js
@@ -7,18 +7,7 @@ angular.module('poaComoVamos', ['poaComoVamos.servicos'])
         templateUrl: '../views/vereadores.html',
         controller: 'VereadoresCtrl',
         resolve: {
-          vereadores: function(Vereadores, $q) {
-
-            var deferred = $q.defer();
-
-            Vereadores.query(function(successData) {
-              deferred.resolve(successData);
-            }, function(errorData) {
-              deferred.reject();
-            });
-
-            return deferred.promise;
-          }
+          vereadores: function(Vereadores) { return Vereadores.query() }
         }
       })
       .otherwise({
