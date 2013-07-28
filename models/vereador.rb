51b788077891998dec1979665aba3e8f6ca462a2
guifroes <guilherme.froes@gmail.com>
2013-07-28 20:48:32 -0300

    atualizando planilha e o model com fotos

--- models/vereador.rb
+++ models/vereador.rb
@@ -1,11 +1,10 @@
 class Vereador
   include MongoMapper::Document
-  
   key :nome, String
   key :partido, String
   key :email, String
   key :telefone, String
-  key :foto, String
   key :twitter, String
-  key :site, String
+  key :facebook, String
+  key :foto, String
 end
