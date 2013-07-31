155c5c186e4c16843239cd6065869ef0679682e7
guifroes <guilherme.froes@gmail.com>
2013-07-31 13:01:01 -0300

    Mudando a uri do mongo para mongolab no heroku

--- environment.rb
+++ environment.rb
@@ -3,7 +3,7 @@ require 'bundler'
 Bundler.setup
 
 
-MongoMapper.setup( { 'mongo' => { 'uri' => ENV['MONGODB_URI'] || 'mongodb://localhost/pcv' } }, 'mongo')
+MongoMapper.setup( { 'mongo' => { 'uri' => ENV['MONGOLAB_URI'] || 'mongodb://localhost/pcv' } }, 'mongo')
 
 base = File.dirname(__FILE__)
 Dir.glob(base + '/models/*.rb').each { |f| require f }
