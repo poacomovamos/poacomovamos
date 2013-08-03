cf36bb5339a7b38ca2390208dd5d5f0c79fbb506
drborges <dborges@thoughtworks.com>
2013-08-03 16:49:32 -0300

    Refactoring no Leitor de presencas em sessoes

--- lib/leitor_resultado_votacao.rb
+++ lib/leitor_resultado_votacao.rb
@@ -4,20 +4,21 @@ require 'open-uri'
 require 'formatador'
 require 'nokogiri'
 
-class LeitorResultadoVotacao
+class LeitorPresencaEmSessoes
 
-  def self.puxar_resultado(data, sessao, tipo_sessao, votacao, tipo_votacao)
+  def self.puxar_presencas(data, sessao, tipo_sessao, votacao, tipo_votacao)
     url = "http://votacoes.camarapoa.rs.gov.br/parlamentares_nome?data=#{data}&sessao=#{sessao}&tiposessao=#{tipo_sessao}&tipovotacao=#{tipo_votacao}&votacao=#{votacao}"
     doc = Nokogiri::HTML(open(url))
-    tabela_votacao = doc.css('.list tr')
-    tabela_votacao.shift
-    tabela_votacao.each do |parlamentar|
-      nome = parlamentar.css('td')[0].text
-      presente = parlamentar.css('td')[2].text != 'Ausente'
-      puts Presenca.create({
+    presencas = doc.css('.list tr:not(:first-of-type)')
+
+    presencas.each do |presenca|
+      informacao_presenca = presenca.css('td')
+      nome_parlamentar = informacao_presenca[0].text
+      presente = informacao_presenca[2].text != 'Ausente'
+      Presenca.create({
         sessao: sessao,
         votacao: votacao,
-        parlamentar: nome,
+        parlamentar: nome_parlamentar,
         presenca: presente
       }).save
     end
