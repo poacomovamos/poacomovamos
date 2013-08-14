d5f415f2e06b6930c3aa7e3f56f1179c78509837
Rafael Lisboa <rafaelzlisboa@gmail.com>
2013-08-13 21:10:10 -0300

    melhorando o logging nas tarefas que puxam as presenças

--- lib/leitor_presenca_sessao.rb
+++ lib/leitor_presenca_sessao.rb
@@ -1,12 +1,11 @@
-# encoding: utf-8
-
 require 'open-uri'
 require 'formatador'
 require 'nokogiri'
 
 class LeitorPresencaEmSessao
+
   def self.puxar_presencas(sessao)
-    url = URI( URI::encode("http://votacoes.camarapoa.rs.gov.br/votacoes?data=#{sessao.data.strftime("%d/%m/%Y %H:%M:%S")}&numero=#{sessao.numero}&tiposessao=#{sessao.tipo}"))
+    url = URI(URI::encode("http://votacoes.camarapoa.rs.gov.br/votacoes?data=#{sessao.data.strftime("%d/%m/%Y %H:%M:%S")}&numero=#{sessao.numero}&tiposessao=#{sessao.tipo}"))
     doc = Nokogiri::HTML(open(url))
     doc.css('.list td:nth-child(5) a').each do |link_votacao|
       link = link_votacao.attribute('href').value
@@ -17,8 +16,10 @@ class LeitorPresencaEmSessao
     end
   end
 
+
   def self.puxar_presenca_votacao(data, sessao, tipo_sessao, votacao, tipo_votacao)
     url = URI(URI::encode("http://votacoes.camarapoa.rs.gov.br/parlamentares_nome?data=#{data.strftime("%d/%m/%Y %H:%M:%S")}&sessao=#{sessao}&tiposessao=#{tipo_sessao}&tipovotacao=#{tipo_votacao}&votacao=#{votacao}"))
+    
     doc = Nokogiri::HTML(open(url))
     presencas = doc.css('.list tr:not(:first-of-type)')
     presencas.each do |presenca|
@@ -26,13 +27,15 @@ class LeitorPresencaEmSessao
       nome_parlamentar = informacao_presenca[0].text
       presente = informacao_presenca[2].text != 'Ausente'
       
-      puts "Salvando presenca - Sessao: #{sessao}, Votacao: #{votacao}, Parlamentar: #{nome_parlamentar}, presenca: #{presente}"
-      Presenca.create({
+      p = Presenca.new({
         sessao: sessao,
         votacao: votacao,
         parlamentar: nome_parlamentar,
         presenca: presente
-      }).save
+      })
+      p.save
+      Formatador.display_line "[blue]salvou presença #{p.inspect}[/]"
     end
   end
+
 end
--- lib/leitor_sessoes.rb
+++ lib/leitor_sessoes.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 require 'open-uri'
 require 'formatador'
 require 'nokogiri'
