d59c14e6f6cd8da24e5be9e585b12ac8dfadfef7
guifroes <guilherme.froes@gmail.com>
2013-09-23 22:15:55 -0300

    ~Requerendo~ as classes da pasta lib

--- environment.rb
+++ environment.rb
@@ -2,8 +2,8 @@ require 'mongo_mapper'
 require 'bundler'
 Bundler.setup
 
-
 MongoMapper.setup( { 'mongo' => { 'uri' => ENV['MONGOLAB_URI'] || 'mongodb://localhost/pcv' } }, 'mongo')
 
 base = File.dirname(__FILE__)
 Dir.glob(base + '/models/*.rb').each { |f| require f }
+Dir.glob(base + '/lib/*.rb').each { |f| require f }
