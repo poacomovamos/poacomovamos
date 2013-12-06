d9624ba2453c4bdbd9972a3db3afae7bca6fbcdf
= <=>
2013-12-06 11:22:04 -0200

    Estrutura para perfil do vereador

---
+++ public/js/controllers/perfil.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('poaComoVamos').
+  controller('PerfilCtrl', ['$scope', 'vereadores', function ($scope, vereadores) {
+
+      //Perfil aqui embaixo
+
+
+      //Projetos aqui embaixo
+
+    };
+}]);
