8d824a5631f1a2d6b76816b3a132a1a029fff4c2
= <=>
2013-12-09 10:58:31 -0200

    TDD para os projetos do perfil do vereador

--- public/js/controllers/perfil.js
+++ public/js/controllers/perfil.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('poaComoVamos').
-  controller('PerfilCtrl', ['$scope', 'perfil', function ($scope, vereadores) {
+  controller('PerfilCtrl', ['$scope', 'voto', function ($scope, voto) {
 
       //Perfil aqui embaixo
 
 
       //Projetos aqui embaixo
-
+      $scope.voto = voto;
 
 }]);
