dd1d408557ce08471e7d475eb47d9c994a0e3d1c
PatriciaDurand <durand_patricia_@hotmail.com>
2013-12-17 10:42:48 -0200

    Criando teste para votos em projetos (banco)

--- models/projeto.rb
+++ models/projeto.rb
@@ -1,8 +1,14 @@
 class Projeto
   include MongoMapper::Document
-  key :nome_projeto, String
-  key :ementa, String
-  key :status, String
-  key :autor, String
-  key :votos, Array
+    key :nome_projeto, String
+    key :ementa, String
+    key :status, String
+    key :autor, String
+    key :votos, Array
+end
+
+class Voto
+    include MongoMapper::Document
+    key :id_vereador, String
+    key :tipo_voto, String
 end
--- test/ruby/models/spec_vereador.rb
+++ test/ruby/models/spec_vereador.rb
@@ -15,13 +15,35 @@ class TestMain < Test::Unit::TestCase
     assert_equal("bernardino@camarapoa.rs.gov.br", Vereador.first(:email => "bernardino@camarapoa.rs.gov.br").email)
   end
 
-=begin
-def test_se_vereador_tem_votos_em_projetos
-  #assert_not_nil(Projeto.find("52aaf15f022884a015c8de90"))
-  assert_equal("Autor", Projeto.first(:autor => "Autor").autor)
+  def test_se_vereador_tem_votos_em_projetos
+      v1 = Voto.create(:id_vereador => 'id', :tipo_voto => 'tipo')
+      v2 = Voto.create(:id_vereador => 'id', :tipo_voto => 'tipo')
+      v3 = Voto.create(:id_vereador => 'id', :tipo_voto => 'tipo')
 
+
+      p = Projeto.new(
+          :nome_projeto => "Projeto1",
+          :ementa => "ementa",
+          :status => "status",
+          :autor => "autor",
+          :votos => [{}
+          ]
+      )
+
+
+    # p = Projeto.new(
+    #     :nome_projeto => "Projeto1",
+    #     :ementa => "ementa"
+    #   ).save
+
+
+    #assert_not_nil(Projeto.find("52aaf15f022884a015c8de90"))
+    #assert_equal("autor", Projeto.first(:autor => "autor").autor)
   end
-=end
+
+  # def test_se_voto_em_projetos
+  #   assert_not_nil(Projeto.first(:votos => "a").votos)
+  # end
 
 
 end
