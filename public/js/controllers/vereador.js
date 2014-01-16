d57fb8e7480d28017d4f74692dd38b8f1757bc2a
maiconrcf <maiconrcf@gmail.com>
2014-01-16 15:05:06 -0200

    Terminado layout da pagina do vereador

--- public/js/controllers/vereador.js
+++ public/js/controllers/vereador.js
@@ -5,24 +5,14 @@ angular.module('poaComoVamos').
   controller('VereadorCtrl', ['$scope', 'vereador', '$routeParams', function ($scope, vereador, $routeParams) {
 
   $scope.initGrafico = function(){
-	 var bar = new RGraph.Bar('cvs', [30,22,10,3])
-		.Set('labels', ['Votou', 'Absteve-se','Rep. Externa','Ausente'])
+	 var bar = new RGraph.Bar('cvs', [30,5,22,10,3])
+		.Set('labels', ['Votou','Não votou', 'Absteve-se','Rep. Externa','Ausente'])
 		.Set('text.color', '#FFFFFF')
 		.Set('colors.sequential', true)
 		.Set('background.grid', false)
-
 		.Draw();
 
-		var colors = [];
-
-		RGraph.each (bar.coords, function (key, value){
-			var x1 = value[0];
-			var y1 = 0;
-			var x2 = value[0] + value[2];
-			var y2 = 0;
-
-			colors[key] = RGraph.LinearGradient(bar, x1, y1, x2, y2, '#00c','blue');
-		});
+		var colors = ['#297FB9'];
 
 		bar.Set('chart.colors', colors);
 		RGraph.Clear(bar.canvas);
