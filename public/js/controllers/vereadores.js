fe95bc627f6e18347a1823059eae000aafe17902
maiconrcf <maiconrcf@gmail.com>
2014-01-20 09:10:57 -0200

    Layout vereadores

--- public/js/controllers/vereadores.js
+++ public/js/controllers/vereadores.js
@@ -9,27 +9,23 @@ angular.module('poaComoVamos').
 
 angular.module('poaComoVamos').
     controller('BuscaCtrl', ['$scope', '$http' , function ($scope, $http) {
-    
 
     $http({method: 'GET', url: '/api/vereador'}).
         success(function(data, status, headers, config) {
             $scope.extrairNomes(data);
         }).
         error(function(data, status, headers, config) {
-       
+            console.log(data + status);
         });
-
 
     $scope.extrairNomes = function(vereadores){
         if(!vereadores) return [];
-
 
         $scope.names = [];
         for (var i = vereadores.length - 1; i >= 0; i--) {
             $scope.names.push(vereadores[i].nome);
         };
     }
-
 }]).
 directive('autoComplete', function($timeout, $location) {
     return function(scope, iElement, iAttrs) {
