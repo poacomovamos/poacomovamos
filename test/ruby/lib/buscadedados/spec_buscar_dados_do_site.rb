c17fe0a4fd14452ef3b8f5a8fac39cafe1132cdb
PatriciaDurand <durand_patricia_@hotmail.com>
2014-02-11 15:12:33 -0200

    Corrigindo build quebrado

--- test/ruby/lib/buscadedados/spec_buscar_dados_do_site.rb
+++ test/ruby/lib/buscadedados/spec_buscar_dados_do_site.rb
@@ -6,5 +6,17 @@ describe BuscarDadosDoSiteDaCamara do
   before(:all) do
 
   end
-
+=begin
+  it 'pega todos links de sessão percorrendo todas as urls' do
+    @buscador_de_sessoes.pega_todos_os_links
+    .should eq(['sessoes/sessao119/index.html', 'sessoes/sessao14/index.html',
+      'sessoes/sessao13/index.html', 'sessoes/sessao12/index.html',
+      'sessoes/sessao11/index.html', 'sessoes/sessao10/index.html',
+      'sessoes/sessao09/index.html', 'sessoes/sessao08/index.html',
+      'sessoes/sessao07/index.html', 'sessoes/sessao06/index.html',
+      'sessoes/sessao05/index.html', 'sessoes/sessao04/index.html',
+      'sessoes/sessao03/index.html', 'sessoes/sessao02/index.html',
+      'sessoes/sessao01/index.html'])
+  end
+=end
 end
--- test/ruby/lib/buscadedados/spec_leitor_de_pagina.rb
+++ test/ruby/lib/buscadedados/spec_leitor_de_pagina.rb
@@ -1,5 +1,5 @@
 require './environment'
-require_relative '../../../../lib/buscadedados/buscador_de_sessao'
+require_relative '../../../../lib/buscadedados/leitor_de_pagina'
 require_relative 'fake_base_page'
 
 describe BuscadorDeSessao do
@@ -37,20 +37,5 @@ describe BuscadorDeSessao do
       'sessoes/sessao12/index.html',
       'sessoes/sessao11/index.html'])
   end
-
-=begin
-  it 'pega todos links de sessão percorrendo todas as urls' do
-    @buscador_de_sessoes.pega_todos_os_links
-    .should eq(['sessoes/sessao119/index.html', 'sessoes/sessao14/index.html',
-      'sessoes/sessao13/index.html', 'sessoes/sessao12/index.html',
-      'sessoes/sessao11/index.html', 'sessoes/sessao10/index.html',
-      'sessoes/sessao09/index.html', 'sessoes/sessao08/index.html',
-      'sessoes/sessao07/index.html', 'sessoes/sessao06/index.html',
-      'sessoes/sessao05/index.html', 'sessoes/sessao04/index.html',
-      'sessoes/sessao03/index.html', 'sessoes/sessao02/index.html',
-      'sessoes/sessao01/index.html'])
-  end
-=end
-
 
 end
